552dd344ed92a8a1534cef688bcf5bf783b03dba
adamekhorejsi <adamhorejsi@email.cz>
2023-10-16 17:26:57 +0200

    v yx

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-let listHodnot = document.querySelectorAll(".cena");
-let minHodnota = 999999;
-let maxHodnota = 0;
-for (index = 0; index < listHodnot.length; index++) {
-    let hodnota = parseInt(listHodnot[index].innerText.replace(" ", ""));
-    if (hodnota >= maxHodnota) {
-        maxHodnota = hodnota;
+    let mainElement = document.querySelector("main");
+    let header = document.querySelector("header");
+    let tlacitkoOdstranit = document.createElement("button.odstranit");
+    let tlacitkoVratit = document.createElement("button.vratit");
+    tlacitkoOdstranit.textContent = "Odstranit";
+    tlacitkoVratit.textContent = "Vratit";
+    tlacitkoOdstranit.style.color = "red";
+    tlacitkoVratit.style.color = "green";
+    mainElement.children.style.display = "flex";
+    header.appendChild(tlacitkoOdstranit);
+    header.appendChild(tlacitkoVratit);
+
+    let odstraneneElementy = [];
+    tlacitkoOdstranit.addEventListener("click", function odstranitElementy() {
+
+        odstraneneElementy = Array.from(mainElement.children); // Uložíme odstraněné elementy do pole
+        for (const element of odstraneneElementy) {
+            mainElement.removeChild(element); // Odstraníme elementy z hlavního elementu
+        }
     }
-    if (hodnota <= minHodnota) {
-        minHodnota = hodnota;
+    );
+
+
+    tlacitkoVratit.addEventListener("click", function vratElementyZpet() {
+
+        for (const element of odstraneneElementy) {
+            mainElement.appendChild(element[0]); // Vrátíme elementy zpět do hlavního elementu
+        }
     }
-}
-console.log(maxHodnota);
-console.log(minHodnota);
+    );
 
 
+
+
+
+});
